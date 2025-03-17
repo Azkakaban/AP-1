19d15a5a7b64649b5aba75e8d423d348dca6d06f
Azkakaban <arrkaban@gmail.com>
2025-03-17 09:23:36 +0700

    Pertemuan 2 Operator

---
+++ pertemuan 2/pertemuan2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+    system("cls");
+    int a,b;
+
+    a = 3;
+    b = 5;
+
+//     int tambah = a + b;
+//     int kurang = a - b;
+//     int kali = a * b;
+//     float koma = a / float(b);
+//     int bagi = a / b;
+//     int modulo = a % b;
+// 
+//     cout<<tambah<<endl;
+//     cout<<kurang<<endl;
+//     cout<<kali<<endl;
+//     cout<<koma<<endl;
+//     cout<<bagi<<endl;
+//     cout<<modulo<<endl;
+
+    // cout<< (a==b) << endl;
+    // cout<< (a>b) << endl;
+    // cout<< (a>=b) << endl;
+    // cout<< (a<b) << endl;
+    // cout<< (a<=b) << endl;
+    // cout<< (a!=b) << endl;
+
+    // cout<<(true && true)<< endl;
+    // cout<<(true && false)<< endl;
+    // cout<<(false && true)<< endl;
+    // cout<<(false && false)<< endl;
+
+    // cout<<(true || true)<< endl;
+    // cout<<(true || false)<< endl;
+    // cout<<(false || true)<< endl;
+    // cout<<(false || false)<< endl;
+
+    // cout<<(!true)<< endl;
+    // cout<<(!false)<< endl;
+
+    // cout<<(5&7)<<endl;
+    // cout<<(5|7)<<endl;
+    // cout<<(5^7)<<endl;
+    // cout<<(~7)<<endl;
+    // cout<<(7<<2)<<endl;
+    // cout<<(7>>2)<<endl;
+
+    // a+=7;
+    // cout<<a<<endl;
+    // a-=7;
+    // cout<<a<<endl;
+    // a*=7;
+    // cout<<a<<endl;
+    // a/=7;
+    // cout<<a<<endl;
+
+    // cout<<a<<endl;
+    // cout<<++a<<endl;
+    // 
+    // cout<<b<<endl;
+    // cout<<++b<<endl;
+
+//     cout<<a<<endl;
+//     cout<<a++<<endl;
+//     cout<<a<<endl;
+// 
+//     cout<<b<<endl;
+//     cout<<b++<<endl;
+//     cout<<b<<endl;
+
+    cout<<a<<endl;
+    cout<<--a<<endl;
+    
+    cout<<b<<endl;
+    cout<<-b<<endl;
+ 
+
+
+}
