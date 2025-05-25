d3b17aeafebf986bef30f3497f7aa80036cc2dc7
Azkakaban <arrkaban@gmail.com>
2025-05-25 17:04:59 +0700

    Latihan3.cpp

--- Pertemuan 4/Latihan3.cpp
+++ Pertemuan 4/Latihan3.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main() {
-    string password, sandi, expected = "";
+    string password, sandi, expected = ""; //deklarasi dan inisialisai expected dengan karakter kosong
 
     cout << "Masukkan kata sandi : ";
-    cin >> password;
+    cin >> password; //input password
 
     cout << "Masukkan kata : ";
-    cin >> sandi;
+    cin >> sandi; //input sandi
 
     // Membuat string "expected" berdasarkan pola tertentu
     for (int i = 0; i < password.length(); i++) {
-        expected += password.substr(0, i + 1);
+        expected += password.substr(0, i + 1); //menambahkan expected dengan karakter dari password indeks ke 0 sampai indeks ke i + 1
     }
 
     // Mengecek apakah input kedua sesuai dengan pola yang diharapkan
     if (expected == sandi) {
-        cout << "Tulisan agen benar" << endl;
+        cout << "Tulisan agen benar" << endl; //output
     } else {
-        cout << "Tulisan agen salah" << endl;
+        cout << "Tulisan agen salah" << endl;//output
     }
 
     return 0;
