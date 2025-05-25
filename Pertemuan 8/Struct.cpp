8be40d1529c5d57727081b4680d9b0e6f6dfdfa2
Azkakaban <arrkaban@gmail.com>
2025-05-25 18:23:11 +0700

    Struct.cpp

--- Pertemuan 8/Struct.cpp
+++ Pertemuan 8/Struct.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 
-struct Alamat {
+struct Alamat { //struct Alamat
     string jalan;
     string kota;
     int kodePos;
@@ -18,19 +18,21 @@ struct Mahasiswa {
 
 int main () {
     system("CLS");
-    Mahasiswa mhs1;
+    Mahasiswa mhs1; //deklarasi
 
+    //inisialisasi
     // mhs1.alamat.jalan = "Jalan Jamin Ginting";
     // mhs1.alamat.kota = "Medan";
     // mhs1.alamat.kodePos = 20155;
 
     // cout << "Alamat : " << mhs1.alamat.jalan << " " << mhs1.alamat.kota << " " << mhs1.alamat.kodePos << endl;
 
-    vector<Mahasiswa> mahasiswa;
+    vector<Mahasiswa> mahasiswa; //vector mahasiswa denan tipe data Mahasiswa
     int n;
     cout << "Masukkan banyak mahasiswa : ";
     cin >> n;
 
+    //input data mahasiswa dengan looping
     for(int i = 0; i < n; i++) {
         cout << "Mahasiswa " << i + 1 << endl;
 
@@ -44,9 +46,10 @@ int main () {
         cout << "Masukkan ipk : ";
         cin >> mhs1.ipk;
 
-        mahasiswa.push_back(mhs1);
+        mahasiswa.push_back(mhs1); //mengisi data mhs1 ke vector mahasiswa
     }
 
+    //Output
     for(int i = 0; i < n; i++){
         cout << "Mahasiswa " << i + 1 << endl;
         cout << "Nama : " << mahasiswa[i].nama << endl;
