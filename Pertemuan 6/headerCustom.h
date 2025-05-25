0f6cd1a563b3ef47f62f7eaec135a9d343445c02
Azkakaban <arrkaban@gmail.com>
2025-05-25 17:50:52 +0700

    headerCustom.h

--- Pertemuan 6/headerCustom.h
+++ Pertemuan 6/headerCustom.h
@@ -1,4 +1,4 @@
-#define u using namespace std;
-#define l <<
-#define o cout
-#define e endl;
+#define u using namespace std; //mengganti fungsi using namespace std; dengan u
+#define l << //mengganti << dengan l
+#define o cout //mengganti cout dengan o
+#define e endl; //mengganti endl; dengan e
