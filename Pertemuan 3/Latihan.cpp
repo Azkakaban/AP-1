3077b50fc2a54b0eb5b0882d79282acf52229981
Azkakaban <arrkaban@gmail.com>
2025-05-25 16:51:55 +0700

    Latihan.cpp

--- Pertemuan 3/Latihan.cpp
+++ Pertemuan 3/Latihan.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int angka;
+    int angka; //deklarasi
     cout << "Masukkan sebuah bilangan bulat: ";
-    cin >> angka;
+    cin >> angka; //input
 
     if (angka % 10 == 0) {
-        cout << angka << " adalah kelipatan dari 10." << endl;
+        cout << angka << " adalah kelipatan dari 10." << endl; //kerjakan jika angka kelipatan 10
     } else if (angka % 5 == 0) {
-        cout << angka << " adalah kelipatan dari 5." << endl;
+        cout << angka << " adalah kelipatan dari 5." << endl; //kerjakan jika angka kelipatan 5
     } else {
-        cout << angka << " bukan kelipatan dari 5 atau 10." << endl;
+        cout << angka << " bukan kelipatan dari 5 atau 10." << endl; //kerjakan jika bukan kelipatan kelipatan 10 dan 5
     }
 
  return 0;
