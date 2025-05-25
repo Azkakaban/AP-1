b31606764e0c202f80ec52437b7bedb389af7aa7
Azkakaban <arrkaban@gmail.com>
2025-05-25 17:27:05 +0700

    LatihanArray.cpp

--- Pertemuan 5/LatihanArray.cpp
+++ Pertemuan 5/LatihanArray.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main() {
 
     // penjumlahan matriks 2 x 2
+    //deklarasi matriks 2 dimensi
     int matriks1[2][2];
     int matriks2[2][2];
     int hasil[2][2];
 
     cout << "Matriks 1 : " << endl;
+    //input nilai Matriks 1 dengan Looping
     for(int i = 0; i < 2; i++) {
         for(int j = 0; j < 2; j++) {
             cout << "Masukkan elemen baris " << i + 1 << " kolom " << j + 1 << " : ";
@@ -17,6 +19,7 @@ int main() {
         }
     }
 
+    //input nilai Matriks 2 dengan Looping
     cout << "Matriks 2 : " << endl;
     for(int i = 0; i < 2; i++) {
         for(int j = 0; j < 2; j++) {
@@ -26,6 +29,7 @@ int main() {
     }
 
     cout << "Hasil Penjumlahan" << endl;
+    //Looping untuk menjumlakan Matriks 1 dan Matriks 2 sekaligus Output
     for (int i = 0; i < 2; i++) {
         for(int j = 0; j < 2; j++) {
             hasil[i][j] = matriks1[i][j] + matriks2[i][j];
