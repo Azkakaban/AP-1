0f5205e9f91673f8e509e6633b407cabde7c1ff3
Azkakaban <arrkaban@gmail.com>
2025-05-25 17:37:41 +0700

    BelajarFungsi.cpp

--- Pertemuan 6/BelajarFungsi.cpp
+++ Pertemuan 6/BelajarFungsi.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama) {
+//fungsi void tanpa pengembalian nilai, seperti procedure jika di dalam Pascal
+void sapa(string nama) { //sapa adalah nama fungsi, dan yang di dalam kurung adah parameter formal fungsi
     cout << "Halo " << nama << "! Selamat belajar C++!" << endl;
 }
 
-int main () {
+int main () { //fungsi Utama untuk Main Program
     string namaPengguna = "Alya";
 
-    sapa(namaPengguna);
+    sapa(namaPengguna); //MengAkses Fungsi sapa dengan parameter aktual namaPengguna
 
     return 0;
 }
